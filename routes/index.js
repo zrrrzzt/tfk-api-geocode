d857e17980bb638e13d839a59f2a7bd067c861d2
Geir Gåsodden <geir.gasodden@pythonia.no>
2015-04-09 21:49:33 +0200

    Latest changes

--- routes/index.js
+++ routes/index.js
@@ -5,17 +5,28 @@ routes = [
   {
     method: 'GET',
     path: '/geocode/{address}',
-    handler: handlers.getGeocode
+    handler: handlers.getGeocode,
+    config: {
+      description:'Get lat/lng from address.'
+    }
   },
   {
     method: 'GET',
     path: '/geocode/convert/UTMtoLL',
-    handler: handlers.convertUTMToLatLng
+    handler: handlers.convertUTMToLatLng,
+    config: {
+      description:'Convert UTM coordinates tol lat/lng. Required params: easting, northing, zoneletter and zonenumber'
+    }
   },
   {
     method: 'GET',
     path: '/geocode/convert/LLtoUTM',
-    handler: handlers.convertLatLngToUTM
+    handler: handlers.convertLatLngToUTM,
+    config: {
+      description:'Convert lat/lng to UTM coordinates. Required params: lat and lng',
+      notes:[],
+      tags:[]
+    }
   }
 ];
 
