4a9f7e2aa5a9b61b27b5a5af10650a96e9312e72
Geir Gåsodden <geir.gasodden@pythonia.no>
2015-04-09 08:50:53 +0200

    Removes need for helpers

--- handlers/index.js
+++ handlers/index.js
@@ -2,19 +2,26 @@
 
 var Wreck = require('wreck');
 var querystring = require('querystring');
-var helpers = require('../helpers');
 var config = require('../config');
 var apiUrl = 'https://maps.googleapis.com/maps/api/geocode/json?';
 var qs = {
   API_KEY:config.API_KEY
 };
 
+function handleReply(err, data, request, reply) {
+  if (err) {
+    reply(err);
+  } else {
+    reply(JSON.parse(data));
+  }
+}
+
 function getGeocode(request, reply) {
   qs.address = request.params.address;
   var url = apiUrl + querystring.stringify(qs);
 
   Wreck.get(url, function(err, res, payload){
-    helpers.handleReply(err, payload, request, reply);
+    handleReply(err, payload, request, reply);
   });
 }
 
