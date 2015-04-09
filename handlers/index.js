477d74220799e5abd66e049ca0e70615568ab39b
Geir Gåsodden <geir.gasodden@pythonia.no>
2015-04-09 09:03:12 +0200

    Implements latlng/UTM converters

--- handlers/index.js
+++ handlers/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var Wreck = require('wreck');
+var Converter = require('wsg84-util');
 var querystring = require('querystring');
 var config = require('../config');
 var apiUrl = 'https://maps.googleapis.com/maps/api/geocode/json?';
@@ -25,4 +26,33 @@ function getGeocode(request, reply) {
   });
 }
 
+function convertLatLngToUTM(request, reply) {
+  var latitude = request.query.lat;
+  var longitude = request.query.lng;
+  var convertded = Converter.LLtoUTM({
+    latitude: latitude,
+    longitude: longitude
+  });
+
+  reply(convertded);
+}
+
+function convertUTMToLatLng(request, reply) {
+  var easting = request.query.easting;
+  var northing = request.query.northing;
+  var zoneNumber = request.query.zone;
+  var converted = Converter.UTMtoLL({
+    easting:easting,
+    northing:northing,
+    zoneNumber:zoneNumber
+  });
+
+  reply(converted);
+
+}
+
 module.exports.getGeocode = getGeocode;
+
+module.exports.convertLatLngToUTM = convertLatLngToUTM;
+
+module.exports.convertUTMToLatLng = convertUTMToLatLng;
